c6bbfa9573ecaf199fa4022abcc7028fe6f2d70b
binoyPeries <binoyperies98@gmail.com>
2025-01-22 14:25:47 +0530

    Add code

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/choreo-poc/submodule-repo
 
 go 1.22.1
 
+replace github.com/choreo-poc/submodule-pvt-nested => ./external
--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"	
+	"fmt"
+
+	submodule "github.com/choreo-poc/submodule-repo/external"
 )
 
 func Hello() string {
 	return "Hello from main repo"
 }
 
-
 func main() {
 	fmt.Println("result from Hello():", Hello())
-	// fmt.Println("result from nestedsubmodule.HelloMsg():", submodule.HelloMsg())
+	fmt.Println("result from nestedsubmodule.HelloMsg():", submodule.HelloMsg())
 }
